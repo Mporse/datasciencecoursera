02d62eb58020b9c43b09c3a0585e6a1a3a1502aa
Mporse <madsporsepedersen@gmail.com>
2018-11-21 21:54:22 +0100

    Code solutions for Week 2 of Course 2

---
+++ Code Excercises/Course 2_R Programming/Week 3_Assignment 2_Lexical Scoping/makeCacheMatrix_FINAL.R
@@ -0,0 +1,13 @@
+makeCacheMatrix <- function(x = matrix(runif(5*5, 1, 10), 5, 5)) {
+    s <- NULL
+    set <- function(y) {
+        x <<- y
+        s <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) s <<- solve
+    getsolve <- function() s
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
+}
